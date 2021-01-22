8124eea25b05b0fb375784f41afc64bc6d655c08
Noemi22cs <77586866+Noemi22cs@users.noreply.github.com>
2021-01-22 13:09:38 -0600

    Add files via upload

---
+++ Métodos_busqueda/src/ordenamiento_externo/Busqueda_binaria.java
@@ -0,0 +1,70 @@
+package ordenamiento_externo;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Noemi
+ */
+public class Busqueda_binaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int op, aux;
+        do {
+            int tamaño = Integer.parseInt(JOptionPane.showInputDialog("Defina el tamaño del arreglo"));
+
+            int arreglo[] = new int[tamaño];
+            for (int i = 0; i < arreglo.length; i++) {
+                arreglo[i] = Integer.parseInt(JOptionPane.showInputDialog((i + 1) + ".Digite un elemento: "));
+
+            }
+
+            // metodo de ordenamiento burbuja 
+            for (int i = 0; i < (tamaño - 1); i++) {
+                for (int j = 0; j < (tamaño - 1); j++) {
+                    if (arreglo[j] > arreglo[j + 1]) {//  si numero actual es mayor al numero siguiente
+                        aux = arreglo[j];
+                        arreglo[j] = arreglo[j + 1];
+                        arreglo[j + 1] = aux;
+
+                    }
+                }
+            }
+
+            //mostrar elementos del arreglo
+            JOptionPane.showMessageDialog(null, "Datos del arreglo");
+            for (int i = 0; i < arreglo.length; i++) {
+                JOptionPane.showMessageDialog(null, "arreglo en la posicion [" + i + "] = " + arreglo[i]);
+            }
+
+            int num_buscado = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un elemento que desea buscar"));
+            int inferior = 0;
+            int centro;
+            int superior = tamaño - 1;
+
+            while (inferior <= superior) {
+                centro = ((superior + inferior) / 2);
+                if (arreglo[centro] == num_buscado) {
+                    JOptionPane.showMessageDialog(null, "el numero se encuentra en la posición:  " + centro);
+                    break;
+                } else if (num_buscado < arreglo[centro]) {
+                    superior = centro - 1;
+                } else {
+                    inferior = centro + 1;
+                }
+            }
+
+            //busqueda BINARIA
+            for (int i = 0; i < arreglo.length; i++) {
+                if (arreglo[i] == num_buscado) {
+                    JOptionPane.showMessageDialog(null, "el numero se encuentra en la posición:  " + i);
+                }
+            }
+            op = Integer.parseInt(JOptionPane.showInputDialog("Desea volver a realizar otra operacion? 1:si 2:no"));
+            JOptionPane.showMessageDialog(null, "¡Programa Finalizada! ");
+        } while (op < 2);
+    }
+    
+}
